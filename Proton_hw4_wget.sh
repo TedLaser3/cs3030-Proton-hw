751fd4560c3b3459511532c81583e344f0fa2993
tedlaser3 <theodorelaser@mail.weber.edu>
2017-02-21 16:01:56 +0000

    Initial Commit for wget and ftp scripts.

---
+++ Proton_hw4_wget.sh
@@ -0,0 +1,24 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: Proton_hw4_wget.sh
+# 
+#         USAGE: ./Proton_hw4_wget.sh 
+# 
+#   DESCRIPTION: the script is responsible for getting the files that we need.
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: THEODORE LASER (), theodorelaser@mail.weber.edu
+#  ORGANIZATION: 
+#       CREATED: 02/21/2017 15:55
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+
+exit 0
+

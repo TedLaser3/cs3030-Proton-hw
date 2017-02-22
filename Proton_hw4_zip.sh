fbf29dee486c15d30a289c63ddcc468bd8e9fa48
rlemmons <robertlemmons@mail.weber.edu>
2017-02-22 00:36:47 +0000

    Init zip psuedocode

---
+++ Proton_hw4_zip.sh
@@ -0,0 +1,27 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: Proton_hw4_zip.sh
+# 
+#         USAGE: ./Proton_hw4_zip.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Rob Lemmons (), robertlemmons@mail.weber.edu
+#  ORGANIZATION: 
+#       CREATED: 02/22/2017 00:34
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+#create zip file
+
+#MOCK_DATA_FILTER_YYYY_MM_DD_HH:MM.zip
+
+
+exit 0
+

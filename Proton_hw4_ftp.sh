ebcefcb4726143a6a22e9a56a55603bac4eb7e38
tedlaser3 <theodorelaser@mail.weber.edu>
2017-02-23 16:34:49 +0000

    Updated readme, near final ftp script.

--- Proton_hw4_ftp.sh
+++ Proton_hw4_ftp.sh
@@ -18,22 +18,51 @@
 #===============================================================================
 
 #set -o nounset                              # Treat unset variables as an error
-user=student1
-pass=Bluesky17
+#user=student1
+#pass=Bluesky17
+
+#getops
+while getopts ":u:p:e:" opt
+do
+	case $opt in
+		u) user=$OPTARG
+			;;
+		p) pass=$OPTARG
+			;;
+		e) email=$OPTARG
+			;;
+	esac
+done
+
 
 #use ftp to connect to the VM
-ftp -n 137.190.19.93 << EOS
-user $user $pass
-ls
-exit
-EOS
+#ftp -n 137.190.19.93 << EOS
+#user $user $pass
+#ls
+#exit
+#EOS
+
+#enter correct directory
+cd temp
 #if user AND password are provided use those
-
+if [[ ! -z $user ]]
+then
+	ftp -n 137.190.19.93 << EOS
+	user $user $pass
+	put need final file name here
+	exit
+EOS
 #else use anonymous login
+else
+	ftp -n 137.190.19.93 << EOS
+	user anonymous $email
+	cd MockData
+	put need final file name here
+	exit
+EOS
+fi
 
-#wget http://icarus.cs.weber.edu/~hvalle/cs3030/MOCK_DATA_2015.tar.gz
-
-
+#return to previous directory
+cd ..
 
 exit 0
-
--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Proton Group for CS3030 Bash Module 5 Homework
 
 1. main wrapper script that contains the other 6 scripts
 
-2. script to get files with wget (Ted)
+2. script to get files with wget
+	usage Proton_hw4_wget.sh <year>
 
 3. script to expand files that script 2 got
 
@@ -13,6 +14,9 @@ Proton Group for CS3030 Bash Module 5 Homework
 
 5. script to compress final filtered file
 
-6. ftp script (Ted)
+6. ftp script
+	usage Proton_hw4_ftp.sh -u <user> -p <pass> -e <email>
+	will upload file to the homedirectory of the user if given
+	otherwise it will upload to the anonymous holding directory
 
 7. script to clean up our mess

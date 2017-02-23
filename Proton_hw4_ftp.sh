611773c61884bc6dbd11c8d829ba2050b1c17b60
tedlaser3 <theodorelaser@mail.weber.edu>
2017-02-23 20:20:43 +0000

    added error out option to ftp script, still needs testing

--- Proton_hw4_ftp.sh
+++ Proton_hw4_ftp.sh
@@ -41,7 +41,7 @@ done
 #ls
 #exit
 #EOS
-
+ts=`date +%Y_%m_%d_%H:%M`
 #enter correct directory
 cd temp
 #if user AND password are provided use those
@@ -49,17 +49,24 @@ if [[ ! -z $user ]]
 then
 	ftp -n 137.190.19.93 << EOS
 	user $user $pass
-	put need final file name here
+	put MOCK_DATA_FILTER_$ts.zip
 	exit
 EOS
+echo "File uploaded to $user home directory"
 #else use anonymous login
-else
+elif [[ ! -z $email ]]
+then
 	ftp -n 137.190.19.93 << EOS
 	user anonymous $email
 	cd MockData
-	put need final file name here
+	put MOCK_DATA_FILTER_$ts.zip
 	exit
 EOS
+echo "File uploaded to /srv/ftp/MockData"
+else
+	echo "please enter either a username and password or an email for anonymous ftp"
+	cd ..
+	exit 1
 fi
 
 #return to previous directory
